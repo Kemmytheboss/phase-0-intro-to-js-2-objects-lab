09d0ed2f5dfda37591c1d4abf6c25c81041cfa43
Verah Mokaya <verahmokaya475@gmail.com>
2025-08-28 16:37:48 +0600

    objects introduction assignemnt done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Verah Mokaya",
+    street: "Mombasa Road",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "title", "manager")
+console.log(updatedEmployee)
+console.log(employee)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]= value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+}
